ad923fe06f11aac15a55f635f5ba688e9a19dac6
daniil789 <pechenkin031199@mail.ru>
2020-11-06 18:05:30 +0500

    Task1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,7 +6,22 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string operation = string.Empty;
+
+            while (operation != "5")
+            {
+                Console.WriteLine($"Выберете номер задания: \n 1.Найти значение выражений  \n 2.");
+
+                operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "1":
+                        var task1 = new Task1();
+                        Console.Clear();
+                        break; 
+                }
+            }
         }
     }
 }
---
+++ Lab1/Task1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    class Task1
+    {
+        public static double EnterNumber()
+        {
+            return Convert.ToDouble(Console.ReadLine());
+
+        }
+
+        public Task1()
+        {
+            string operation = string.Empty;
+
+            while (operation != "5")
+            {
+                Console.WriteLine("Выберете выражение, которое хотите вычислить: \n 1.m+--n \n 2.m++<--n \n 3.--m>n— \n 4.Arccos(x+x2) \n 5.Выбрать другое задание");
+
+                operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "1":
+                        N1();
+                        Console.Clear();
+                        break;
+                    case "2":
+                        N2();
+                        Console.Clear();
+                        break;
+                    case "3":
+                        N3();
+                        Console.Clear();
+                        break;
+                    case "4":
+                        N4();
+                        Console.Clear();
+                        break;
+                    case "5":
+                        return;
+                }
+            }
+
+        }
+
+        private static void N1()
+        {
+            string YN = string.Empty;
+            while (YN != "N" && YN != "n")
+            {
+                try
+                {
+                    Console.WriteLine("Вычисляется выражение m+--n, введите m...");
+                    var m = EnterNumber();
+
+                    Console.WriteLine("Вычисляется выражение m+--n, введите n...");
+                    var n = EnterNumber();
+
+                    Console.WriteLine($"m+--n = {m + --n} \n");
+                }
+                catch
+                {
+                    Console.WriteLine("Введены некорректные данные.\n");
+                }
+
+                Console.WriteLine("Попробовать ещё раз?(Y/N)");
+                YN = Console.ReadLine();
+
+            }
+        }
+
+        private static void N2()
+        {
+            string YN = string.Empty;
+            while (YN != "N" && YN != "n")
+            {
+                try
+                {
+                    Console.WriteLine("Вычисляется выражение m++<--n, введите m...");
+                    var m = EnterNumber();
+
+                    Console.WriteLine("Вычисляется выражение m++<--n, введите n...");
+                    var n = EnterNumber();
+
+                    Console.WriteLine($"m++<--n = {m++ < --n} \n");
+                }
+                catch
+                {
+                    Console.WriteLine("Введены некорректные данные.\n");
+                }
+
+                Console.WriteLine("Попробовать ещё раз?(Y/N)");
+                YN = Console.ReadLine();
+
+            }
+        }
+
+        private static void N3()
+        {
+            string YN = string.Empty;
+            while (YN != "N" && YN != "n")
+            {
+                try
+                {
+                    Console.WriteLine("Вычисляется выражение --m>n—, введите m...");
+                    var m = EnterNumber();
+
+                    Console.WriteLine("Вычисляется выражение --m>n—, введите n...");
+                    var n = EnterNumber();
+
+                    Console.WriteLine($"m++<--n = {--m > n--} \n");
+                }
+                catch
+                {
+                    Console.WriteLine("Введены некорректные данные.\n");
+                }
+
+                Console.WriteLine("Попробовать ещё раз?(Y/N)");
+                YN = Console.ReadLine();
+
+            }
+        }
+
+        private static void N4()
+        {
+            string YN = string.Empty;
+            while (YN != "N" && YN != "n")
+            {
+                try
+                {
+                    Console.WriteLine("Вычисляется выражение Arccos(x+x2), введите x...");
+                    var x = EnterNumber();
+
+                    Console.WriteLine($"Arccos(x+x2) = {Math.Acos(x+x*x)} \n");
+                }
+                catch
+                {
+                    Console.WriteLine("Введены некорректные данные.\n");
+                }
+
+                Console.WriteLine("Попробовать ещё раз?(Y/N)");
+                YN = Console.ReadLine();
+
+            }
+        }
+
+    }
+}
